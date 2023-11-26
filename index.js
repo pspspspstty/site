7d014c6c633ac5832bf4b254096269c5998e6790
ondotcom <aesonvtran@gmail.com>
2023-11-26 13:15:36 -0800

    finish prob

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import '@react95/icons/icons.css';
-import "98.css";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import '@react95/icons/icons.css';
+// import "98.css";
 
-const App = () => {
-    const [count, setCount] = React.useState(0);
-    return (
-      <div style={{ width: 300 }} className="window">
-        <div className="title-bar">
-          <div className="title-bar-text">Counter</div>
-          <div className="title-bar-controls">
-            <button aria-label="Minimize" />
-            <button aria-label="Maximize" />
-            <button aria-label="Close" />
-          </div>
-        </div>
+// alert("test1")
+
+// const App = () => {
+//     const [count, setCount] = React.useState(0);
+//     return (
+//       <div style={{ width: 300 }} className="window">
+//         <div className="title-bar">
+//           <div className="title-bar-text">Counter</div>
+//           <div className="title-bar-controls">
+//             <button aria-label="Minimize" />
+//             <button aria-label="Maximize" />
+//             <button aria-label="Close" />
+//           </div>
+//         </div>
   
-        <div className="window-body">
-          <p style={{ textAlign: "center" }}>Current count: {count}</p>
-          <div className="field-row" style={{ justifyContent: "center" }}>
-            <button onClick={() => setCount(count + 1)}>+</button>
-            <button onClick={() => setCount(count - 1)}>-</button>
-            <button onClick={() => setCount(0)}>0</button>
-          </div>
-        </div>
-      </div>
-    );
-  };
+//         <div className="window-body">
+//           <p style={{ textAlign: "center" }}>Current count: {count}</p>
+//           <div className="field-row" style={{ justifyContent: "center" }}>
+//             <button onClick={() => setCount(count + 1)}>+</button>
+//             <button onClick={() => setCount(count - 1)}>-</button>
+//             <button onClick={() => setCount(0)}>0</button>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   };
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-    >
-      <div style={{ width: 350 }}>
-        <App />
-      </div>
-    </div>
-  </React.StrictMode>,
-  rootElement
-);
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <div
+//       style={{
+//         height: "100vh",
+//         display: "flex",
+//         justifyContent: "center",
+//         alignItems: "center"
+//       }}
+//     >
+//       <div style={{ width: 350 }}>
+//         <App />
+//       </div>
+//     </div>
+//   </React.StrictMode>,
+//   rootElement
+// );
 
-alert('hey');
+// alert("test2")
